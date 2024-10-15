a322e3c24d0a5b363641a88d4860554d29a7785f
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2024-10-15 10:56:52 +0300

    add read more functionality

--- 03-tours/src/Components/Tour.tsx
+++ 03-tours/src/Components/Tour.tsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
 import { ITour } from "../interfaces";
 import styles from "./Tour.module.scss";
 
 function Tour({ image, info, name, price }: ITour) {
+  const [isExtended, setIsExtended] = useState(true);
+  const words = info.split(" ");
+  const limit = 30;
   return (
     <div className={styles.tour}>
       <span className={styles.price}>{price}</span>
       <img src={image} alt={name} />
       <div className={styles.tourContent}>
         <h2>{name}</h2>
-        <p>{info}</p>
+        <div>
+          <p>{isExtended ? words.join(" ") : words.slice(0, limit).join(" ")}</p>
+
+          <button type="button" className="btn-link" onClick={() => setIsExtended(!isExtended)}>
+            {isExtended ? "Read Less" : "Read More"}
+          </button>
+        </div>
         <button className="btn">Not Intersted</button>
       </div>
     </div>
