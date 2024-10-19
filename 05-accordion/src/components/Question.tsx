11b600b4981d35f599fd760b37d9b91a067bca35
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2024-10-19 13:35:35 +0300

    update alternative way of is extended

--- 05-accordion/src/components/Question.tsx
+++ 05-accordion/src/components/Question.tsx
@@ -4,19 +4,18 @@ import styles from "./Question.module.scss";
 import { FaCircleMinus, FaCirclePlus } from "react-icons/fa6";
 
 function Question({ id, title, info }: IQuestion) {
-  const [isExtended, setIsExtended] = useState(false);
-  const [activeId, setActiveId] = useState(null);
+  const [activeId, setActiveId] = useState<number | null>(null);
 
   const isActive = activeId === id;
 
-  function handleActiveId(id) {
+  function handleActiveId(id: number) {
     const newActiveId = isActive ? null : id;
     setActiveId(newActiveId);
   }
 
   return (
     <div className={styles.question}>
-      <div className={styles.title} onClick={() => handleActiveId(id)}>
+      <div className={styles.title} onClick={() => id !== undefined && handleActiveId(id)}>
         <h2>{title}</h2>
         <button className={styles.btn}>{isActive ? <FaCircleMinus /> : <FaCirclePlus />}</button>
       </div>
