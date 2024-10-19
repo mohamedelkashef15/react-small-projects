3a1d4d78400822fb801e9aab2416138d6b043105
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2024-10-19 13:30:31 +0300

    Alternative way or isExtended

--- 05-accordion/src/components/Question.tsx
+++ 05-accordion/src/components/Question.tsx
@@ -3,20 +3,24 @@ import { IQuestion } from "../interfaces";
 import styles from "./Question.module.scss";
 import { FaCircleMinus, FaCirclePlus } from "react-icons/fa6";
 
-function Question({ title, info }: IQuestion) {
+function Question({ id, title, info }: IQuestion) {
   const [isExtended, setIsExtended] = useState(false);
+  const [activeId, setActiveId] = useState(null);
 
-  function hadnleToggle() {
-    setIsExtended(!isExtended);
+  const isActive = activeId === id;
+
+  function handleActiveId(id) {
+    const newActiveId = isActive ? null : id;
+    setActiveId(newActiveId);
   }
 
   return (
     <div className={styles.question}>
-      <div className={styles.title} onClick={hadnleToggle}>
+      <div className={styles.title} onClick={() => handleActiveId(id)}>
         <h2>{title}</h2>
-        <button className={styles.btn}>{isExtended ? <FaCircleMinus /> : <FaCirclePlus />}</button>
+        <button className={styles.btn}>{isActive ? <FaCircleMinus /> : <FaCirclePlus />}</button>
       </div>
-      {isExtended ? <p>{info}</p> : ""}
+      {isActive && <p>{info}</p>}
     </div>
   );
 }
