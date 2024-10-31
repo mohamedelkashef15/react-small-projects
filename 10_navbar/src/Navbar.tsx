26a253766413bbfb8267c2c7e081c8a9294f394d
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2024-10-31 13:30:07 +0300

    add static part

---
+++ 10_navbar/src/Navbar.tsx
@@ -0,0 +1,36 @@
+import styles from "./Navbar.module.scss";
+import { socials, links } from "./data";
+
+function Navbar() {
+  return (
+    <div className={styles.navbar}>
+      <nav>
+        <h1>
+          Mohamed <span className="text-primary">Elkashef</span>
+        </h1>
+
+        <ul>
+          {links.map((link) => {
+            return (
+              <li key={link.id}>
+                <a href={link.url}>{link.text}</a>
+              </li>
+            );
+          })}
+        </ul>
+
+        <div className={styles.social}>
+          {socials.map((social) => {
+            return (
+              <a href={social.url} key={social.id}>
+                {social.icon}
+              </a>
+            );
+          })}
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
